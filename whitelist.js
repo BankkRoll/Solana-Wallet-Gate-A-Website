8789d4caaf6395ed5a9209752998c0d19662fac8
Bankk <106103625+BankkRoll@users.noreply.github.com>
2022-10-26 17:48:27 -0400

    Updated whitelist.js
    added array to show how to input list of wallets for multiple users to be "wallet-gated"

--- whitelist.js
+++ whitelist.js
@@ -1,3 +1,8 @@
 module.exports=[
+    '7A8xUQtzoWbFm19WZmeUiGMqfWJLcuCEZiEqBzBd7eb8',
+    '7A8xUQtzoWbFm19WZmeUiGMqfWJLcuCEZiEqBzBd7eb8',
+    '7A8xUQtzoWbFm19WZmeUiGMqfWJLcuCEZiEqBzBd7eb8',
+    '7A8xUQtzoWbFm19WZmeUiGMqfWJLcuCEZiEqBzBd7eb8',
+    '7A8xUQtzoWbFm19WZmeUiGMqfWJLcuCEZiEqBzBd7eb8',
     '7A8xUQtzoWbFm19WZmeUiGMqfWJLcuCEZiEqBzBd7eb8'
 ]
